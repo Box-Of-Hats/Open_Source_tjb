ffe421b0cf44d70ee2f1fea2800d9ce673b5d864
Vault-hunter <ja1ke@hotmail.co.uk>
2016-03-13 11:53:24 +0000

    Added a load of stuff

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,41 @@
 from flask import Flask, render_template, request, flash
 from forms import DataForm
 
- 
+from flask import request, jsonify
+
 app = Flask(__name__) 
 #app.secret_key = 'development key' #Can be changed. Should be hard to guess. Allows authentication
 
-
-app = Flask(__name__)      
- 
 @app.route('/')
 def home():
   return render_template('home.html')
  
-@app.route('/one')
+@app.route('/results', methods=['GET', 'POST'])
 def one():
-    return render_template('one.html')
+    if request.method == 'POST':
+        imgs = ['static/img/eximg1.png',
+                'static/img/eximg2.png',
+                'static/img/eximg3.png']
+        return render_template('results.html', form=request.form, imglist=imgs)
+
+    elif request.method == 'GET':
+        return render_template('dataform.html')
 
 @app.route('/datarequest', methods=['GET', 'POST'])
-def contact():
+def input_form():
+    def build_page(request):
+            return request.form['name']
+
     form = DataForm(csrf_enabled=False)
 
     if request.method == 'POST':
-        return 'Name: {} Username: {}'.format(request.form['name'], request.form['username'])
+        #return 'Name: {} Username: {}'.format(request.form['name'], request.form['username'])
         #return 'Form posted.'
+        return build_page(request)
  
     elif request.method == 'GET':
         return render_template('dataform.html', form=form)
+
 
 
 if __name__ == '__main__':
--- test_flask.py
+++ test_flask.py
@@ -1,16 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    pagehtml = """This is the homepage.<br/>
-    <a href='http://127.0.0.1:5000/two'>Page Two</a>"""
-    return pagehtml
-
-
-@app.route("/two")
-def hellotwo():
-    return "Hello World two!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+from flask import Flask, render_template
+ 
+app = Flask(__name__)      
+ 
+@app.route('/')
+def home():
+  return render_template('templates/home.html')
+ 
+if __name__ == '__main__':
+  app.run(debug=True)
